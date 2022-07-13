7dd23035b0e72af3bdc5bf8f373b1dbd682aeceb
Darko Pranjic <darkopranjic.4@gmail.com>
2022-07-13 15:04:47 +0200

    working  star

--- src/graph/Astar.ts
+++ src/graph/Astar.ts
@@ -1,4 +1,7 @@
-import { IOsmNode } from '../types/osm-read';
+import FastPriorityQueue from "fastpriorityqueue";
+import { IOsmNode, TPointsToNode } from "../types/osm-read";
+import { calculateTravelTime, greatCircleVec } from "../utils/distance";
+import { getPenaltyTransition } from "../utils/routing";
 
 export function heuristic(a: IOsmNode, b: IOsmNode) {
   // euclidian distance
@@ -27,9 +30,7 @@ export const haversine = (a: IOsmNode, b: IOsmNode) => {
   const Δφ = ((b.lat - a.lat) * Math.PI) / 180;
   const Δλ = ((b.lon - a.lon) * Math.PI) / 180;
 
-  const calculation =
-    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const calculation = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
   const c = 2 * Math.atan2(Math.sqrt(calculation), Math.sqrt(1 - calculation));
 
   const d = R * c; // in metres
@@ -37,7 +38,7 @@ export const haversine = (a: IOsmNode, b: IOsmNode) => {
   return d;
 };
 
-class SearchNode {
+export class SearchNode {
   node: IOsmNode;
   previous: SearchNode | null;
   gScore: number;
@@ -45,6 +46,8 @@ class SearchNode {
   hScore: number;
   visited: boolean;
   closed: boolean;
+  highway?: string;
+  polyline?: [number, number][];
 
   constructor(node: IOsmNode) {
     this.gScore = 0;
@@ -67,112 +70,146 @@ function removeFromArray(arr: SearchNode[], el: IOsmNode) {
   return arr;
 }
 
+class PriorityQueue {
+  private queue: SearchNode[];
+  constructor() {
+    this.queue = [];
+  }
+
+  add(node: SearchNode) {
+    this.queue.push(node);
+    this.queue.sort((a, b) => {
+      if (a.fScore < b.fScore) return -1;
+      else if (a.fScore > b.fScore) return 1;
+      return 0;
+    });
+  }
+
+  dequeue() {
+    const first = this.queue.shift();
+    return first;
+  }
+
+  isEmpty() {
+    return this.queue.length;
+  }
+
+  find(searchNode: TPointsToNode) {
+    return this.queue.find(sn => {
+      if (sn.node.geohash === searchNode.node.geohash) {
+        return sn;
+      }
+    });
+  }
+
+  get size() {
+    return this.queue.length;
+  }
+}
 export class AStar {
-  constructor() {}
+  avgSpeed: number;
+  constructor(avgSpeed: number) {
+    this.avgSpeed = avgSpeed;
+  }
 
   search(start: IOsmNode, end: IOsmNode) {
-    let openedSet: SearchNode[] = [];
-    let closedSet = new Map<string, boolean>();
+    let openSet: SearchNode[] = [new SearchNode(start)]; //array containing unevaluated grid points
+    let closedSet: SearchNode[] = []; //array containing completely evaluated grid points
     let path = [];
     let current;
+    let routeFound = false
 
-    const startNode = new SearchNode(start);
-
-    openedSet.push(startNode);
-
-    while (openedSet.length) {
-      // search node
-      let lowestFIndex = 0;
-
-      // openedSet should be imlemented as priority queue
-      // find the node with the least f on  the open list, call it "q"
-      // pop q off the open list
-      for (const index in openedSet) {
-        if (openedSet[index].fScore < openedSet[lowestFIndex].fScore) {
-          lowestFIndex = Number(index);
+    while (openSet.length > 0) {
+      //assumption lowest index is the first one to begin with
+      let lowestIndex = 0;
+      for (let i = 0; i < openSet.length; i++) {
+        if (openSet[i].fScore < openSet[lowestIndex].fScore) {
+          lowestIndex = i;
         }
       }
+      let current = openSet[lowestIndex];
 
-      current = openedSet[lowestFIndex];
-
+      // kruzni koji bi trebao gledadti na 3840440595 i 625256
+        // ne gleda na 3840440595
+        if(current.node.id === "2287019228"){
+          console.log("")
+        }
+        if(current.node.id === "262892997" || current.node.id === "3659845144"){
+          console.log("")
+        }
+        if(current.node.id === "262892996" || current.node.id === "3928793758"){
+          console.log("")
+        }
+        if(current.node.id === "5043815480"){
+          console.log("")
+        }
+        if(current.node.id === "2287019214"){
+          console.log("")
+        }
+        if(current.node.id === "625256"){
+          console.log("")
+        }
+        if(current.node.id === "3840440576"){
+          console.log("")
+        }
+        if(current.node.id === "3840440595"){
+          console.log("")
+        }
+        if(current.node.id === "3840440610"){
+          console.log("")
+        }
+        if(current.node.id === "3840440602"){
+          console.log("")
+        }
+  
       if (current.node.geohash === end.geohash) {
-        // console.log("Done!", current, end);
-        break;
+        let temp = current;
+        path.push(temp);
+        while (temp.previous) {
+          path.push(temp.previous);
+          temp = temp.previous;
+        }
+        console.log("DONE!");
+        console.log("openSet!", openSet.length);
+        console.log("closedSet!", closedSet.length);
+        // return the traced path
+        return {
+          route: path.reverse().map(sNode => [sNode.node.lat, sNode.node.lon])
+        };
       }
+  
+      //remove current from openSet
+      openSet.splice(lowestIndex, 1);
+      //add current to closedSet
+      closedSet.push(current);
+  
+      let neighbors = current.node.pointsToNodeSimplified;
+      if(!neighbors) break;
 
-      // openedSet = removeFromArray(openedSet, current.node);
-      openedSet.splice(lowestFIndex, 1);
-      openedSet = openedSet;
-
-      closedSet.set(current.node.geohash as string, true);
-
-      // generate q's 8 successors and set their parents to q
-      for (const toNodeConnection of current.node.pointsToNodeSimplified || []) {
-        const newSearchNode = openedSet.find((sn) => sn.node.geohash === toNodeConnection.node.geohash);
-
-        const neighbor =
-          newSearchNode || new SearchNode(toNodeConnection.node);
-        const neighbortraveltime = toNodeConnection.travelTime as number;
-
-        if (!closedSet.get(neighbor.node.geohash  as string)) {
-          let tempG = current.gScore + neighbortraveltime;
-          // f(n) = g(n) + f(n)
-          // g(n) is the cost of the path from the start node to n,
-          // h(n) is a heuristic function that estimates the cost of the cheapest path from n to the goal.
-
-          let newPath = false;
-
-          if (newSearchNode) {
-            if (tempG < neighbor.gScore) {
-              neighbor.gScore = tempG;
-              newPath = true;
-            }
-          } else {
-            newPath = true;
-            neighbor.gScore = tempG;
-            openedSet.push(neighbor);
+      for (let i = 0; i < neighbors.length; i++) {
+        const connection = neighbors[i]
+        let neighbor = new SearchNode(neighbors[i].node);
+  
+        if (!closedSet.find(n => n.node.id === connection.nodeId)) {
+          let possibleG = current.gScore + (connection.distance as number);
+  
+          if (!openSet.find(n => n.node.id === connection.nodeId)) {
+            openSet.push(neighbor);
+          } else if (possibleG >= neighbor.gScore) {
+            continue;
           }
-
-          // update neighbourgh only if g is better than previous one
-          if (newPath) {
-            neighbor.previous = current;
-
-            neighbor.hScore = haversine(neighbor.node, end);
-            neighbor.fScore = neighbor.gScore + neighbor.hScore;
-          }
+  
+          neighbor.gScore = possibleG;
+          neighbor.hScore = greatCircleVec(neighbor.node.lat, neighbor.node.lon, end.lat, end.lon);
+          neighbor.fScore = neighbor.gScore + neighbor.hScore;
+          neighbor.previous = current;
         }
       }
+
     }
-
-    let temp = current;
-
-    console.log("openedSet", openedSet.length);
-    console.log("closedSet", closedSet.size);
-
-    path = [temp];
-
-    while (temp?.previous) {
-      if (temp?.previous) {
-        path.push(temp.previous);
-        temp = temp.previous;
-      }
-    }
-
-    // console.log(path);
-
-    let distance = 0;
-    let traveltime = 0;
-
-    console.log("distance", distance);
-    console.log("traveltime", traveltime);
-
+    
     return {
-      route: path.map((sn) => [sn?.node.lat, sn?.node.lon]) as [number,number][],
-      // visitedNodes: closedSet.map((node) => ({
-      //   ...node,
-      //   pointsTo: (node.pointsTo as IOsmNode[]).map((p) => p.id),
-      //   partOfWays: node.partOfWays.map((w) => w.id),
-      // })),
-    };
+      route: []
+    }
   }
 }
